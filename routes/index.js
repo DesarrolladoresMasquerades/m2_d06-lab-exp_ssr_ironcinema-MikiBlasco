520cf0d30ce2c44e7179fd41de80634bad8b3084
Miki Blasco <blascom@7@gmail.com>
2022-02-13 11:59:07 +0100

    LAB DONE

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/movies', (req, res, next) => {
     Movie.find()
     .then(moviesFromDB =>{
         //console.log(`these are the movies that has been FOUND for moviesFromDB ----> `, moviesFromDB)
-        res.render('movies/movies-list.hbs', {movies: moviesFromDB})
+        res.render('movies/movies-list.hbs', {movies:moviesFromDB})
     })
     .catch(error => console.log('there is an error finding moviesFromDB and rendereing them ', error))
 });
@@ -22,7 +22,7 @@ router.get('/movies/:id/details', (req, res, next) => {
     Movie.findById(id)
     .then(movieTarget => {
         console.log('this is the movie the user wants to get rendered the details ---> ', movieTarget)
-        res.render('movies/movie-details.hbs', {movies: movieTarget})
+        res.render('movies/movie-details.hbs', {movieTarget})
     })
     .catch(error => console.loog('there is an error trying to find and render the targeted movie ', error))
 });
